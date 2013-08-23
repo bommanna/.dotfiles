da194629f6fd02e264e399ae4578e9ebd5905a4c
Matthieu Monsch <ninecoast@gmail.com>
2013-08-23 16:55:13 -0700

    jedi. better install and submodule scripts

--- bin/install.sh
+++ bin/install.sh
@@ -1,40 +1,57 @@
 #!/bin/bash
 
-# TODO: check if mac
+BREW_FORMULAE='bash git tmux ctags ack python vim coreutils node ruby wget fortune'
+NODE_PACKAGES='coffee-script coffeelint'
+PYTHON_PACKAGES='virtualenv nose ipython jedi'
 
-# install (or update/upgrade) Homebrew and formulae
-if command -v brew | read; then
-  brew update
+if [ "$(uname)" == "Darwin" ]; then
+  # Mac OS X
+  if command -v brew | read; then
+    echo 'Updating homebrew...'
+    brew update
+  else
+    echo 'Installing homebrew...'
+    ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+  fi
+  echo 'Installing homebrew formulae...'
+  for FORMULA in $BREW_FORMULAE; do
+    if brew list $FORMULA 2>&1 1>/dev/null | read; then
+      brew install $FORMULA
+    else
+      brew upgrade $FORMULA 2>/dev/null || echo "$FORMULA already up-to-date."
+    fi
+  done
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  # Linux
+  echo 'TODO'
+  exit 0
 else
-  ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+  echo 'Sorry, not compatible for your system.' >&2
+  exit 1
 fi
 
-FORMULAE='git tmux ctags ack python vim coreutils node ruby'
-for FORMULA in $FORMULAE; do
-  if brew list $FORMULA 2>&1 1>/dev/null | read; then
-    brew install $FORMULA
-  else
-    brew upgrade $FORMULA
-  fi
-done
-
-# install interesting node packages
-PACKAGES='coffee-script coffeelint'
-for PACKAGE in $PACKAGES; do
+echo 'Installing node packages...'
+for PACKAGE in $NODE_PACKAGES; do
   npm install -g $PACKAGE
 done
 
-# clone dotfiles git repository
+echo 'Installing python packages...'
+for PACKAGE in $PYTHON_PACKAGES; do
+  pip install --upgrade $PACKAGE
+done
+
 cd ~
 if [ -e '.dotfiles' ]; then
+  echo 'Updating dotfiles repository...'
   cd .dotfiles
   git pull origin master
 else
+  echo 'Cloning dotfiles repository...'
   git clone 'https://github.com/mtth/.dotfiles.git'
   cd .dotfiles
 fi
 
-# run dotfile binaries
+echo 'Running dotfiles binaries...'
 BINARIES='link.sh submodules.sh'
 for BINARY in $BINARIES; do
   bash "bin/$BINARY"
--- bin/submodules.sh
+++ bin/submodules.sh
@@ -2,11 +2,6 @@
 
 # Download missing submodules and update any existing ones
 # Currently only used for Vim plugins
-#
-# To uninstall all submodules, run from repository root:
-# $ git submodule deinit .
-# $ rm -r .gitmodules
-# $ for i in src/config/vim/bundle/*; do git rm --cached $i; done
 
 # Array of Vim plugin git repositories
 VIM_SUBMODULES=(
@@ -16,9 +11,8 @@ VIM_SUBMODULES=(
 'https://github.com/mtth/cursorcross.vim.git'
 'https://github.com/Lokaltog/vim-easymotion.git'
 'https://github.com/tpope/vim-fugitive.git'
-'https://github.com/sjl/gundo.vim.git'
-'https://github.com/michaeljsmith/vim-indent-object.git'
 'https://github.com/LaTeX-Box-Team/LaTeX-Box.git'
+'https://github.com/michaeljsmith/vim-indent-object.git'
 'https://github.com/mtth/locate.vim.git'
 'https://github.com/mtth/scratch.vim.git'
 'https://github.com/tpope/vim-surround.git'
@@ -26,23 +20,47 @@ VIM_SUBMODULES=(
 'https://github.com/mtth/taglist.vim.git'
 'https://github.com/scrooloose/syntastic.git'
 'https://github.com/jmcantrell/vim-virtualenv.git'
+'https://github.com/SirVer/ultisnips.git'
+)
+
+# Unused modules currently
+OTHER_VIM_MODULES=(
+'https://github.com/davidhalter/jedi-vim.git'
+'https://github.com/sjl/gundo.vim.git'
 )
 
 # Directory where Vim plugins will be installed
 VIM_PLUGINS_DIR='../src/config/vim/bundle'
 
+# Main
+
 REPOSITORY_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 ABS_VIM_SUBMODULES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd $VIM_PLUGINS_DIR && pwd )"
 VIM_SUBMODULES_DIR=${ABS_VIM_SUBMODULES_DIR#$REPOSITORY_ROOT/}
+declare -A ACTIVE_VIM_SUBMODULES
 
 cd $REPOSITORY_ROOT
 
-echo 'Checking for missing submodules...'
-for SUBMODULE in "${VIM_SUBMODULES[@]}"; do
-  REPOSITORY=${SUBMODULE##*/}
+echo 'Checking for new submodules...'
+for SUBMODULE_URL in "${VIM_SUBMODULES[@]}"; do
+  REPOSITORY=${SUBMODULE_URL##*/}
   SUBMODULE_DIR="$VIM_SUBMODULES_DIR/${REPOSITORY%.*}"
   if [ ! -e $SUBMODULE_DIR ]; then
-    git submodule add $SUBMODULE $SUBMODULE_DIR
+    echo "Installing ${SUBMODULE_DIR}..."
+    git submodule add --force $SUBMODULE_URL $SUBMODULE_DIR
+  fi
+  ACTIVE_VIM_SUBMODULES["${SUBMODULE_DIR}"]=1
+done
+
+echo 'Checking for deleted submodules...'
+VIM_SUBMODULE_PATHS="$VIM_SUBMODULES_DIR/*"
+for SUBMODULE_DIR in $VIM_SUBMODULE_PATHS; do
+  if [ ! ${ACTIVE_VIM_SUBMODULES[$SUBMODULE_DIR]} ]; then
+    SUBMODULE_NAME="${SUBMODULE_DIR##*/}"
+    echo "Uninstalling ${SUBMODULE_NAME}..."
+    git submodule deinit $SUBMODULE_DIR
+    git rm $SUBMODULE_DIR
+    sed -i.bak "/${SUBMODULE_NAME}/d" .gitmodules && rm .gitmodules.bak
   fi
 done
 
